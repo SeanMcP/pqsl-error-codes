e9d213cceae973eddfc6451e87357bc0d2c8a4f8
Sean McPherson <git@seanmcp.com>
2020-03-06 07:04:52 -0500

    Initial commit
    This is most of the way there.

---
+++ script.js
@@ -0,0 +1,31 @@
+// Scraping https://www.postgresql.org/docs/8.2/errcodes-appendix.html
+
+classesByCode = {}, obj = {}
+
+document.querySelector('table.CALSTABLE tbody').querySelectorAll('tr').forEach(row => {
+  let cells = row.querySelectorAll('td')
+  
+  if (cells.length === 1) {
+    let cellText = cells[0].textContent
+    let [classCode, classTitle] = cellText.split(' —\n')
+    classCode = classCode.split(' ')[1]
+    classesByCode[classCode] = {
+      code: classCode,
+      title: classTitle
+    }
+  } else {
+    let [codeCell, meaningCell, constantCell] = cells
+    
+    let code = codeCell.textContent
+    
+    let thisClass = classesByCode[code.slice(0, 2)]
+    
+    obj[code] = {
+      constant: constantCell.textContent,
+      meaning: meaningCell.textContent,
+      class: thisClass
+    }
+  }
+})
+
+console.log(obj)
