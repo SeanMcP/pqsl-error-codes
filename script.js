eff73f6bb14fb2f19c7a7dd6e6bef14cdce6d7f1
Sean McPherson <git@seanmcp.com>
2020-03-06 12:11:38 -0500

    Update for psql@12

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-// Scraping https://www.postgresql.org/docs/8.2/errcodes-appendix.html
+// Scraping https://www.postgresql.org/docs/current/errcodes-appendix.html
 
-classesByCode = {}, obj = {}
+(classesByCode = {}), (obj = {});
 
-document.querySelector('table.CALSTABLE tbody').querySelectorAll('tr').forEach(row => {
-  let cells = row.querySelectorAll('td')
-  
-  if (cells.length === 1) {
-    let cellText = cells[0].textContent
-    let [classCode, classTitle] = cellText.split(' —\n')
-    classCode = classCode.split(' ')[1]
-    classesByCode[classCode] = {
-      code: classCode,
-      title: classTitle.trim()
+document
+  .querySelector('table[summary="PostgreSQL Error Codes"]')
+  .querySelectorAll("tr")
+  .forEach(row => {
+    let cells = row.querySelectorAll("td");
+
+    if (cells.length) {
+      if (cells.length === 1) {
+        let cellText = cells[0].textContent;
+        let [classCode, classTitle] = cellText.split(" — ");
+        classCode = classCode.split(" ")[1];
+        classesByCode[classCode] = {
+          code: classCode,
+          title: classTitle.trim()
+        };
+      } else {
+        let [codeCell, conditionCell] = cells;
+
+        let code = codeCell.textContent;
+
+        let thisClass = classesByCode[code.slice(0, 2)];
+
+        obj[code] = {
+          condition: conditionCell.textContent,
+          class: thisClass
+        };
+      }
     }
-  } else {
-    let [codeCell, meaningCell, constantCell] = cells
-    
-    let code = codeCell.textContent
-    
-    let thisClass = classesByCode[code.slice(0, 2)]
-    
-    obj[code] = {
-      constant: constantCell.textContent,
-      meaning: meaningCell.textContent,
-      class: thisClass
-    }
-  }
-})
+  });
 
-console.log(obj)
+console.log(obj);
 
-copy(obj)
+copy(obj);
